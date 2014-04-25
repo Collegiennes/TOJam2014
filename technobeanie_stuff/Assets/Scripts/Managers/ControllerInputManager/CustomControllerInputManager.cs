7ac7a5ae3a51e27e993c220f27482aee6312a74e
technobeanie <plpoirier@technobeanie.com>
2014-04-25 14:36:32 -0400

    Hand controllers
    We can now control 2 hands.

--- technobeanie_stuff/Assets/Scripts/Managers/ControllerInputManager/CustomControllerInputManager.cs
+++ technobeanie_stuff/Assets/Scripts/Managers/ControllerInputManager/CustomControllerInputManager.cs
@@ -28,10 +28,6 @@ public partial class ControllerInputManager
 	#region ControllerInputManager Implementation
 	partial void PostInputDetection()
 	{
-		// Controllers.
-		AddKeyboard1();
-		AddMouse();
-
 		// Aliases.
 		SetAliases();
 	}
@@ -44,34 +40,9 @@ public partial class ControllerInputManager
 	#endregion
 	
 	#region Private Functions
-	private void AddKeyboard1()
-	{
-		Dictionary<KeyboardController.eSimulatedAxis, string> axisMap = new Dictionary<KeyboardController.eSimulatedAxis, string>();
-		axisMap.Add(KeyboardController.eSimulatedAxis.LEFT_LEFT_JOYSTICK, "a");
-		axisMap.Add(KeyboardController.eSimulatedAxis.RIGHT_LEFT_JOYSTICK, "d");
-		axisMap.Add(KeyboardController.eSimulatedAxis.UP_LEFT_JOYSTICK, "w");
-		axisMap.Add(KeyboardController.eSimulatedAxis.DOWN_LEFT_JOYSTICK, "s");
-		axisMap.Add(KeyboardController.eSimulatedAxis.LEFT_RIGHT_JOYSTICK, "left");
-		axisMap.Add(KeyboardController.eSimulatedAxis.RIGHT_RIGHT_JOYSTICK, "right");
-		axisMap.Add(KeyboardController.eSimulatedAxis.UP_RIGHT_JOYSTICK, "up");
-		axisMap.Add(KeyboardController.eSimulatedAxis.DOWN_RIGHT_JOYSTICK, "down");
-
-		Dictionary<KeyboardController.eKeyboardButtonId, string> buttonIds = new Dictionary<KeyboardController.eKeyboardButtonId, string>();
-		buttonIds.Add(KeyboardController.eKeyboardButtonId.ACTION, "z");
-		buttonIds.Add(KeyboardController.eKeyboardButtonId.BACK, "x");
-
-		KeyboardController keyboard1 = new KeyboardController(axisMap, buttonIds);
-		AddController(keyboard1);
-	}
-	
-	private void AddMouse()
-	{
-		MouseController mouse = new MouseController();
-		AddController(mouse);
-	}
-
 	private void SetAliases()
 	{
+		AddButtonAlias((BaseController.eButtonId)Xbox360Controller.eXbox360ButtonId.A, eButtonAliases.GRAB.ToString());
 	}
 	#endregion
 }
---
+++ technobeanie_stuff/Assets/Scripts/Scenes/Views/MainGame/Hand.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Hand : MonoBehaviour 
+{
+	#region Members and properties
+	// constants
+	private const float SPEED = 500.0f;
+	
+	// enums
+	
+	// public
+	public ControllerInputManager.eControllerId m_ControllerId = ControllerInputManager.eControllerId.CONTROLLER_01;
+	
+	// protected
+	
+	// private
+	
+	// properties
+	#endregion
+	
+	#region Unity API
+	private void Update()
+	{
+		Vector2 movement = ControllerInputManager.Instance.GetLeftJoystick(m_ControllerId);
+		transform.position += new Vector3(movement.x, movement.y, 0.0f) * (Time.deltaTime * SPEED);
+	}
+	#endregion
+	
+	#region Public Methods
+	#endregion
+	
+	#region Protected Methods
+	#endregion
+	
+	#region Private Methods
+	#endregion
+}
