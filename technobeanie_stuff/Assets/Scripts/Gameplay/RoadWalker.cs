395496b7d0f54d1b754b05fd8d482ee7c3e60f48
Renaud Bédard <renaud.bedard@gmail.com>
2014-04-25 22:06:29 -0400

    Splinewalker!

---
+++ technobeanie_stuff/Assets/Scripts/Gameplay/RoadWalker.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+class RoadWalker : MonoBehaviour
+{
+    public Spline RoadToWalk;
+    public float Speed = 1;
+
+    float distanceFromStart;
+
+    void Update()
+    {
+        if (!RoadToWalk) return;
+
+        distanceFromStart += 0.1f;
+
+        transform.position = RoadToWalk.RoadCenterAt(distanceFromStart);
+    }
+}
--- technobeanie_stuff/Assets/Scripts/Gameplay/Spline.cs
+++ technobeanie_stuff/Assets/Scripts/Gameplay/Spline.cs
@@ -11,23 +11,35 @@ class Spline : MonoBehaviour
     public float Width = 0.5f;
 
     public Mesh SplineMesh;
+    public SplineNode[] CachedNodes;
 
     void Awake()
     {
         var meshFilter = GetComponent<MeshFilter>();
         SplineMesh = (meshFilter.sharedMesh = new Mesh());
+        CachedNodes = GetComponentsInChildren<SplineNode>().OrderBy(x => x.name).ToArray();
+        Rebuild();
+    }
+
+    public void Update()
+    {
+        TryRebuild();
     }
 
     float cacheChecksum;
-    public void Update()
+    void TryRebuild()
     {
+        if (!Application.isEditor)
+            return;
+
+        // safeguard if it's deleted from the editor
         if (GetComponent<MeshFilter>().sharedMesh == null)
             Awake();
 
         // go through child nodes and check if checksum matches
         float newChecksum = 0;
-        var childNodes = GetComponentsInChildren<SplineNode>().OrderBy(x => x.name).ToArray();
-        foreach (var node in childNodes)
+        CachedNodes = GetComponentsInChildren<SplineNode>().OrderBy(x => x.name).ToArray();
+        foreach (var node in CachedNodes)
             newChecksum += Vector3.Dot(node.transform.position, Vector3.one);
         cacheChecksum += Width;
         cacheChecksum += SegmentRate;
@@ -35,24 +47,56 @@ class Spline : MonoBehaviour
         if (cacheChecksum != newChecksum)
         {
             // only rebuild mesh if checksum changed
-            Rebuild(childNodes);
+            Rebuild();
             //Debug.Log("Rebuilt!");
             cacheChecksum = newChecksum;
         }
     }
 
-    public void Rebuild(SplineNode[] nodes)
+    public Vector3 RoadCenterAt(float distanceFromStart)
+    {
+        var segments = Math.Round(CachedNodes.Length * 100 * SegmentRate);
+
+        var nodePositions = CachedNodes.Select(x => x.transform.localPosition).ToArray();
+
+        float lengthSeen = 0;
+        for (int i = 0; i <= segments; i++)
+        {
+            var s = i / (float)segments;
+
+            var center = MathfPlus.BSpline(nodePositions, s);
+            Vector3 nextCenter = MathfPlus.BSpline(nodePositions, (i + 1) / (float)segments);
+
+            var segmentLength = Vector3.Distance(nextCenter, center);
+            lengthSeen += segmentLength;
+
+            if (lengthSeen > distanceFromStart)
+            {
+                float distanceAtLastSegment = lengthSeen - segmentLength;
+                return Vector3.Lerp(center, nextCenter, (distanceFromStart - distanceAtLastSegment) / (lengthSeen - distanceAtLastSegment));
+            }
+        }
+
+        // fallback : at distance = 0
+        return RoadCenterAt(0);
+    }
+
+    public float RoadTotalLength { get; private set; }
+
+    void Rebuild()
     {
         var vertices = new List<Vector3>();
         var triangles = new List<int>();
         var uv = new List<Vector2>();
 
-        var nodePositions = nodes.Select(x => x.transform.localPosition).ToList();
+        var nodePositions = CachedNodes.Select(x => x.transform.localPosition).ToArray();
 
         SplineMesh.Clear();
         SplineMesh.subMeshCount = 1;
 
-        var segments = Math.Round(nodes.Length * 100 * SegmentRate);
+        RoadTotalLength = 0;
+
+        var segments = Math.Round(CachedNodes.Length * 100 * SegmentRate);
 
         float accumulatedTexCoord = 0;
         for (int i = 0; i <= segments; i++)
@@ -66,8 +110,9 @@ class Spline : MonoBehaviour
             var tangent = Vector3.Cross(diff, Vector3.up);
 
             var c = vertices.Count;
-
-            accumulatedTexCoord += Vector3.Distance(nextCenter, center) * 0.1f;
+            var segmentLength = Vector3.Distance(nextCenter, center);
+            RoadTotalLength += segmentLength;
+            accumulatedTexCoord += segmentLength * 0.1f;
 
             vertices.Add(center - tangent * Width); uv.Add(new Vector2(0, accumulatedTexCoord));
             vertices.Add(center + tangent * Width); uv.Add(new Vector2(1, accumulatedTexCoord));
