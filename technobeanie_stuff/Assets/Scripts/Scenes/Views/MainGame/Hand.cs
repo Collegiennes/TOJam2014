e5d254ad29e64e1f339afdb617edcac76689e804
technobeanie <plpoirier@technobeanie.com>
2014-04-25 20:45:25 -0400

    Grab objects in the Test Scene,.

--- technobeanie_stuff/Assets/Scripts/Scenes/Views/MainGame/Hand.cs
+++ technobeanie_stuff/Assets/Scripts/Scenes/Views/MainGame/Hand.cs
@@ -7,7 +7,7 @@ public class Hand : MonoBehaviour
 	#region Members and properties
 	// constants
 	private const float SPEED = 800.0f;
-	private const float GRABBED_OFFSET = -500.0f;
+	private const float GRABBED_OFFSET = -250f;
 	private const float RELEASED_OFFSET = 0.0f;
 	
 	// enums
@@ -21,14 +21,14 @@ public class Hand : MonoBehaviour
 	// protected
 	
 	// private
-	private Collider m_GrabbedObstacle = null;
+	private ObstacleHandle m_GrabbedObstacleHandle = null;
 	private HingeJoint m_GrabbedObstacleJoin = null;
 	
 	// properties
 	#endregion
 	
 	#region Unity API
-	private void Update()
+	private void FixedUpdate()
 	{
 		// Movements.
 		Vector2 movement = ControllerInputManager.Instance.GetLeftJoystick(m_ControllerId);
@@ -37,33 +37,29 @@ public class Hand : MonoBehaviour
 		// Buttons.
 		if (ControllerInputManager.Instance.GetButton(m_ControllerId, ControllerInputManager.eButtonAliases.GRAB.ToString()))
 		{
-			m_Asset.spriteId = m_Asset.GetSpriteIdByName(m_HandGrabbedAssetName);
-			transform.localPosition = new Vector3(transform.localPosition.x, transform.localPosition.y, GRABBED_OFFSET);
-
 			GrabObstacle();
 		}
 		else
 		{
-			m_Asset.spriteId = m_Asset.GetSpriteIdByName(m_HandEmptyAssetName);
-			transform.localPosition = new Vector3(transform.localPosition.x, transform.localPosition.y, RELEASED_OFFSET);
-
-			ReleaseObstacle();
+			StartCoroutine("ReleaseObstacle");
 		}
 	}
 
 	private void OnTriggerEnter(Collider other) 
 	{
-		if (m_GrabbedObstacle == null && IsAnObstacle(other))
+		ObstacleHandle handle = other.GetComponent<ObstacleHandle>();
+		if (m_GrabbedObstacleHandle == null && handle != null)
 		{
-			m_GrabbedObstacle = other;
+			m_GrabbedObstacleHandle = handle;
 		}
 	}
 
 	private void OnTriggerExit(Collider other) 
 	{
-		if (other == m_GrabbedObstacle)
+		ObstacleHandle handle = other.GetComponent<ObstacleHandle>();
+		if (handle != null && handle == m_GrabbedObstacleHandle)
 		{
-			m_GrabbedObstacle = null;
+			m_GrabbedObstacleHandle = null;
 		}
 	}
 	#endregion
@@ -77,26 +73,31 @@ public class Hand : MonoBehaviour
 	#region Private Methods
 	private void GrabObstacle()
 	{
-		if (m_GrabbedObstacle != null && m_GrabbedObstacleJoin == null)
+		m_Asset.spriteId = m_Asset.GetSpriteIdByName(m_HandGrabbedAssetName);
+		transform.localPosition = new Vector3(transform.localPosition.x, transform.localPosition.y, GRABBED_OFFSET);
+
+		if (m_GrabbedObstacleHandle != null && m_GrabbedObstacleJoin == null)
 		{
-			m_GrabbedObstacle.transform.position = transform.position;
+			m_GrabbedObstacleHandle.m_Obstacle.transform.position = transform.position - (m_GrabbedObstacleHandle.transform.position - m_GrabbedObstacleHandle.m_Obstacle.transform.position);
 
 			m_GrabbedObstacleJoin = gameObject.AddComponent<HingeJoint>();
-			m_GrabbedObstacleJoin.connectedBody = m_GrabbedObstacle.rigidbody;
+			m_GrabbedObstacleJoin.connectedBody = m_GrabbedObstacleHandle.m_Obstacle.rigidbody;
+			m_GrabbedObstacleJoin.axis = new Vector3(0.0f, 0.0f, 1.0f);
+			m_GrabbedObstacleJoin.anchor = m_GrabbedObstacleHandle.transform.localPosition;
 		}
 	}
 	
-	private void ReleaseObstacle()
+	private IEnumerator ReleaseObstacle()
 	{
 		if (m_GrabbedObstacleJoin != null)
 		{
 			Destroy(m_GrabbedObstacleJoin);
 		}
-	}
-	
-	private bool IsAnObstacle(Collider collider)
-	{
-		return (collider.gameObject.layer == LayerMask.NameToLayer("Death") || collider.gameObject.layer == LayerMask.NameToLayer("DeathNoCollision"));
+		
+		yield return null;
+		
+		m_Asset.spriteId = m_Asset.GetSpriteIdByName(m_HandEmptyAssetName);
+		transform.localPosition = new Vector3(transform.localPosition.x, transform.localPosition.y, RELEASED_OFFSET);
 	}
 	#endregion
 }
